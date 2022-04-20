1e2b264556c2a8a2bf588291509eddbff7d7006a
freaklucas <lucas.oliveira.freak@gmail.com>
2022-04-19 23:50:37 -0300

    add conexao com mongodb

--- trip/server.js
+++ trip/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongo = require("mongodb").MongoClient;
 
+const url = "mongodb://localhost:27017";
+
 const app = express();
 
 app.post("/trip", (req, res) => {});
@@ -10,3 +12,16 @@ app.post("/expense", (req, res) => {});
 app.get("/expenses", (req, res) => {});
 
 app.listen(3000, () => console.log("Server ready"));
+
+let db;
+
+mongo.connect(url, {
+  userNewParser: true,
+  useUnifiedTopology: true,
+},
+(err, client)) => {
+    if(err) {
+        console.log(err)
+    }
+    db = client.db("tripconst");
+};
