faaa4de40292f23f5fa83e63f1094e93ac4cdd1c
freaklucas <lucas.oliveira.freak@gmail.com>
2022-06-27 20:09:24 -0300

    module 1

---
+++ struture/aula1/app.js
@@ -0,0 +1,6 @@
+const { People } = require('./mod1');
+
+const p = new People('João', 20);
+
+
+console.log(p);
---
+++ struture/aula1/mod1.js
@@ -0,0 +1,8 @@
+class People {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+exports.People = People;
