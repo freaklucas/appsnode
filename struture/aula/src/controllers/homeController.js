ff51cd2a00dbdf45c9dc65cb545beffa59a5bc88
freaklucas <lucas.oliveira.freak@gmail.com>
2022-07-09 18:11:19 -0300

    primeiro model de teste

--- struture/aula/src/controllers/homeController.js
+++ struture/aula/src/controllers/homeController.js
@@ -1,3 +1,13 @@
+const HomeModel = require('../models/HomeModel');
+
+HomeModel.create({
+  title: 'Primeiro titulo',
+  description: 'Primeira descrição'
+})
+  .then(data => console.log(data))
+  .catch(e => console.log(e));
+
+
 exports.paginaInicial = (req, res) => {
   res.render('index');
   
