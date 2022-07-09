4672c01340bcb9174260511e72a1c54dcd1c3202
freaklucas <lucas.oliveira.freak@gmail.com>
2022-07-09 18:15:20 -0300

    separando ordem do controller

--- struture/aula/src/controllers/homeController.js
+++ struture/aula/src/controllers/homeController.js
@@ -1,13 +1,3 @@
-const HomeModel = require('../models/HomeModel');
-
-HomeModel.create({
-  title: 'Primeiro titulo',
-  description: 'Primeira descrição'
-})
-  .then(data => console.log(data))
-  .catch(e => console.log(e));
-
-
 exports.paginaInicial = (req, res) => {
   res.render('index');
   
