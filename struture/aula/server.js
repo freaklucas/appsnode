597a952affa42124c4ce50b7512cb134f9f44fcc
freaklucas <lucas.oliveira.freak@gmail.com>
2022-07-09 17:44:57 -0300

    connection + arquivo .env

--- struture/aula/server.js
+++ struture/aula/server.js
@@ -1,10 +1,27 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('conectando com Banco de dados...');
+    app.emit('pronto');
+  }).catch(e => console.log(e));
+
 const routes = require('./routes');
 const path = require('path');
-const { middlewareGlobal } = require('./src/middlewares/mid');
+const {
+  middlewareGlobal
+} = require('./src/middlewares/mid');
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
@@ -15,9 +32,10 @@ app.set('view engine', 'ejs');
 app.use(middlewareGlobal);
 
 app.use(routes);
+app.on('pronto', () => {
+  app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+  });
 
-app.listen(3000, () => {
-  console.log('Acessar http://localhost:3000');
-  console.log('Servidor executando na porta 3000');
 });
-
