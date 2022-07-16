855a949ec4afeed642ae65e103c0fac3829cd7c3
freaklucas <lucas.oliveira.freak@gmail.com>
2022-07-16 16:56:48 -0300

    brincando de csrf

--- struture/aula/server.js
+++ struture/aula/server.js
@@ -20,15 +20,19 @@ const flash = require('connect-flash');
 
 const routes = require('./routes');
 const path = require('path');
+const helmet = require('helmet');
+const csrf = require('csurf');
 const {
-  middlewareGlobal
+  middlewareGlobal, checkCsrfError, csrfMiddleware
 } = require('./src/middlewares/mid');
+
 
 app.use(express.urlencoded({
   extended: true
 }));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(helmet());
 
 
 const sessionOptions = session({
@@ -49,8 +53,12 @@ app.use(flash());
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+app.use(csrf());
+
 //middleware proprio
 app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 
 app.use(routes);
 app.on('pronto', () => {
--- struture/aula/src/middlewares/mid.js
+++ struture/aula/src/middlewares/mid.js
@@ -10,5 +10,15 @@ exports.middlewareGlobal = (req, res, next) => {
     console.log('Passando pelo middleware global');
 
     next();
+}
 
+exports.checkCsrfError = function (err, req, res, next) {
+    if (err && 'EBADCSRFTOKEN' === err.code ) {
+        return res.render('404');
+    }
+}
+
+exports.csrfMiddleware = function (req, res, next) {
+    res.locals.csrfToken = req.csrfToken();
+    next();
 }
